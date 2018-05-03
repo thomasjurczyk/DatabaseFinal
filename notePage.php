dd955bb7e7136e2e9bc9ce198fe85be393199939
VyacheslavSidorov <slavicsidz@gmail.com>
2018-05-03 17:26:09 -0500

    testCommit

---
+++ notePage.php
@@ -0,0 +1,43 @@
+<?php 
+
+session_start();
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Notes</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+</head>
+
+
+
+<body>
+
+<h1>Notes</h1>
+<div class="container-fluid">
+<div class="row">
+<div class="notesContainer">
+<div>
+<?php echo $_SESSION["rows"][2]; ?>
+
+</div>
+
+
+
+</div>
+</div>
+</div>
+</body>
+
+</html>
