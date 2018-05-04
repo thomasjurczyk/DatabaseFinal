fd753200f48203d27987834c0f5bcc2980c0b5a9
VyacheslavSidorov <slavicsidz@gmail.com>
2018-05-04 12:44:01 -0500

    123123

--- readNotes.php
+++ readNotes.php
@@ -4,7 +4,14 @@ $username = "epiz_21487047"; // user name is provided with Account Information o
 $password = "fPnWEWthqmFx";  // password is the one provided with Account Information. It is not login password.
 $dbname = "epiz_21487047_databasefinaldatabase";  // database name is listed with database in control panel
 
+echo <<<EOT
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+EOT;
 session_start();
 if($_SESSION['uname']==null)
 {
@@ -14,11 +21,13 @@ if($_SESSION['uname']==null)
 $uname=$_SESSION['uname'];
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 if (!$conn) {
     die("<p>Connection failed: " . mysqli_connect_error() . "</p>");
 }
 $sql = "SELECT * FROM Notes WHERE Username=\"$uname\"";
 $result = mysqli_query($conn,$sql);
+$row_cnt = $result->num_rows;
 
 if (mysqli_num_rows($result) > 0) {
     $resultArray=array();
@@ -26,11 +35,37 @@ if (mysqli_num_rows($result) > 0) {
         array_push($resultArray,array("Username"=>$row["Username"],"TimeCreated"=>$row["TimeCreated"],"Note"=>$row["Note"]));
     }
     $_SESSION["AllNotes"]=$resultArray;
-    echo $_SESSION["AllNotes"][0]["Note"];
-}
-else
+for ($x = 0; $x <= $row_cnt; $x++) {
+    echo "<div class=\"container-fluid noteContainer\">";
+    echo $_SESSION["AllNotes"][$x]["Note"];
+    echo "</div>";
+    echo "<div class=\"container-fluid timeContainer\">";
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][0];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][1];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][2];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][3];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][4];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][5];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][6];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][7];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][8];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][9];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][10];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][11];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][12];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][13];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][14];
+    echo $_SESSION["AllNotes"][$x]["TimeCreated"][15];
+    echo "</div>";
+    echo "<br>";
+    }
+    }
+    else
 {
     $_SESSION['notes']=null;
     echo "<p>No notes</p>";
 }
+
+
+
 ?>
