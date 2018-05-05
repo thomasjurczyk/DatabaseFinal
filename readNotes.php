48e86a8e20713bf64ea87bc74a2a7c8e9561bbc1
VyacheslavSidorov <slavicsidz@gmail.com>
2018-05-04 21:14:46 -0500

    CSSupdate

--- readNotes.php
+++ readNotes.php
@@ -4,12 +4,16 @@ $username = "epiz_21487047"; // user name is provided with Account Information o
 $password = "fPnWEWthqmFx";  // password is the one provided with Account Information. It is not login password.
 $dbname = "epiz_21487047_databasefinaldatabase";  // database name is listed with database in control panel
 
+
+
+
 echo <<<EOT
-<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" type="text/css" href="readNoteStyle.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
 
 EOT;
 session_start();
@@ -38,7 +42,8 @@ if (mysqli_num_rows($result) > 0) {
         array_push($resultArray,array("Username"=>$row["Username"],"TimeCreated"=>$row["TimeCreated"],"Note"=>$row["Note"]));
     }
     $_SESSION["AllNotes"]=$resultArray;
-    echo "<form action=\"addNote.html\" method=\"post\"><input class=\"button\" type=\"hidden\" name=\"key\"><input type=\"submit\" value=\"ADD\"></form>";
+    echo "<h1 style=\"padding-bottom: 40px; font-size: 40px;\">Notes</h1>";
+    echo "<form style=\"padding-bottom: 40px; border-radius: 5px;\" id=\"addNote\" action=\"addNote.html\" method=\"post\"><input class=\"button\" type=\"hidden\" name=\"key\"><input type=\"submit\" value=\"ADD\"></form>";
 for ($x = 0; $x <= $row_cnt - 1; $x++) {
     echo "<div class=\"container-fluid noteContainer\">";
     echo $_SESSION["AllNotes"][$x]["Note"];
@@ -49,7 +54,7 @@ for ($x = 0; $x <= $row_cnt - 1; $x++) {
     echo "</div>";
     echo "<div class=\"container-fluid timeContainer\">";
 
-    echo "<form action=\"handleDelete.php\" method=\"post\"><input class=\"button\" type=\"hidden\" name=\"key\" value=\"$tempDate\"><input type=\"submit\" value=\"DELETE\"></form>";
+    echo "<form style=\"padding-bottom: 0px; margin-bottom: 2px; padding-top: 10px;\" action=\"handleDelete.php\" method=\"post\"><input class=\"button\" type=\"hidden\" name=\"key\" value=\"$tempDate\"><input type=\"submit\" value=\"DELETE\"></form>";
 
         echo "<form action=\"handleUpdate.php\" method=\"post\"><input class=\"button\" type=\"hidden\" name=\"key\" value=\"$tempDate\"><input type=\"submit\" value=\"UPDATE\"></form>";
 
@@ -63,8 +68,4 @@ for ($x = 0; $x <= $row_cnt - 1; $x++) {
     echo "<p>No notes</p>";
             echo "<form action=\"addNote.html\" method=\"post\"><input class=\"button\" type=\"hidden\" name=\"key\"><input type=\"submit\" value=\"ADD\"></form>";
 }
-
-
-
-
 ?>
